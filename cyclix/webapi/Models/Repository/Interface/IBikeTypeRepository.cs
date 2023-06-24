4c7e4031053b6086fca9998e64669afd3c1a1617
codedive <rsramsharma25@gmail.com>
2023-06-24 19:44:18 +0530

    Initial Commit
    Initial Commit

---
+++ cyclix/webapi/Models/Repository/Interface/IBikeTypeRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace webapi.Models.Repository.Interface
+{
+    public interface IBikeTypeRepository
+    {
+        Task<List<BikeType>> GetBikeTypes();
+    }
+}
---
+++ cyclix/webapi/Models/ServiceData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace webapi.Models;
+
+public partial class ServiceData
+{
+    public int ServiceDataId { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Street { get; set; }
+
+    public string? Number { get; set; }
+
+    public string? PostCode { get; set; }
+
+    public string? Location { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? ProblemDefinition { get; set; }
+
+    public decimal? CostCeiling { get; set; }
+
+    public int? BikeTypeId { get; set; }
+
+    public int? BrandId { get; set; }
+
+    public bool? OptedForStandardService { get; set; }
+
+    public int? StandardServiceId { get; set; }
+
+    public bool? IsElectricDriveExist { get; set; }
+
+    public virtual BikeType? BikeType { get; set; }
+
+    public virtual Brand? Brand { get; set; }
+
+    public virtual StandardService? StandardService { get; set; }
+}
